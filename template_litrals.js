e55ba4c611e30b382fcdcedb9af84726d27fe51b
Shakeel, Asim <Asim.Shakeel@bshg.com>
2022-03-09 14:45:03 +0530

    template_string

---
+++ template_litrals.js
@@ -0,0 +1,10 @@
+function getYear(){
+    const year = new Date().getFullYear();
+
+    //return 'Year is '+ year
+    // template string.
+    return `Year is ${year}`;
+}
+console.log(getYear());
+
+console.log(`Hey there "Asim" how are you!  hope you are 'fine'`);
